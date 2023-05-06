21ed535f3b3120b77c3b4691dca1da5609ddda41
harsh <harshjoshihj3382565@gmail.com>
2023-05-06 16:30:03 +0530

    first commit

---
+++ Packagemain.js
@@ -0,0 +1,17 @@
+import Package from "./Package";
+
+export default function  Packagemain(prop) {
+  return (
+<div className="routes">
+    <h1> We are Providing Best Packages</h1>
+    <div className="routes-part1">
+<Package title="Mumbai Flights" image="./images/bg.jpg" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+<Package title="Mumbai Flights" image="./images/booknow.png" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+<Package title="Mumbai Flights" image="./images/booknow.png" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+<Package title="Mumbai Flights" image="./images/booknow.png" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+<Package title="Mumbai Flights" image="./images/booknow.png" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+<Package title="Mumbai Flights" image="./images/booknow.png" route=" Goa, Delhi, Bangalore, Ahmedabad"/>
+    </div>
+</div>
+     )
+}
